814a8f4b43bac7dcaa9efa618cf320d01134ed28
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-28 21:15:40 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,13 @@
-class Model
+class Dog 
+  attr_accessor :name,:breed
+  @@all =[]
+  def initialize(name,breed)
+    @name=name
+    @breed=breed
+    @@all << self
+  end  
+  def self.all
+    @@all
+  end    
 
 end
